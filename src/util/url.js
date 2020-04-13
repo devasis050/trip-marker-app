534c01f00daa43b6534b60891a4443047e490683
devasis <devasis.majhi@gmail.com>
2020-04-13 18:14:42 +0100

    Update url.js

--- src/util/url.js
+++ src/util/url.js
@@ -1,9 +1,9 @@
 
 const FB_CLIENT_ID = '212634969918380';
-const REDIRECT_URL = 'http://localhost:3000/oauth/redirect'; //'https://tripmarker.herokuapp.com/oauth/redirect'; //'http://localhost:3000/oauth/redirect';
+const REDIRECT_URL = 'https://tripmarker.herokuapp.com/oauth/redirect'; //'http://localhost:3000/oauth/redirect';
 const RESPONSE_TYPE = 'code';
 const FB_OAUTH_URL = 'https://www.facebook.com/v6.0/dialog/oauth';
-const TRIP_MARKER_SERVRE_URL = 'http://localhost:8080'; //'https://tripmarker-server.herokuapp.com'; //'http://localhost:8080';
+const TRIP_MARKER_SERVRE_URL = 'https://tripmarker-server.herokuapp.com'; //'http://localhost:8080';
 
 const getPlaceSearchUrl = (text) => {
     return `${TRIP_MARKER_SERVRE_URL}/search?place=${text}`;
